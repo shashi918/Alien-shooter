c8168e4c2614cf76e50aff493929df52f8d23777
shashi918 <sr0298037@gmail.com>
2023-12-10 08:39:45 +0530

    Frame Rate 60hz

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,7 +8,8 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
-
+        
+        self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((1366,768))
         pygame.display.set_caption("Alien Invasion")
 
@@ -22,6 +23,8 @@ class AlienInvasion:
 
             # Make the most recently drawn screen visible
             pygame.display.flip()
+            self.clock.tick(60)
+            
 
 
 if __name__ == '__main__':
