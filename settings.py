9807a36ace9e4083363cfbf1dabe1d47814a4e0f
shashi918 <sr0298037@gmail.com>
2023-12-26 23:49:47 +0530

    Move settings in another file

---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    """A Class store all setting for Alien Invasion"""
+
+
+    def __init__(self):
+        """Initialize  the game settings."""
+        #Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
+
